eed63c51a04aaad9fe94377021aab438bea462e6
Benia Morgan <bmorganware@gmail.com>
2024-09-19 08:29:22 -0400

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Benia Morgan-Ware on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
