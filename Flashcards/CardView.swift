9edc27c4ce98c1a29901eab905c5f783d2318159
Benia Morgan <bmorganware@gmail.com>
2024-09-19 22:14:23 -0400

    Completed card rotating animation

--- Flashcards/CardView.swift
+++ Flashcards/CardView.swift
@@ -9,24 +9,32 @@ import SwiftUI
 
 struct CardView: View {
     var card: Card
-    @State var isBackVisible: Bool = false {
-        didSet {
-            degrees = isBackVisible ? 180 : 0
-        }
+    @State var isBackVisible: Bool = false
+    var degrees: Double {
+       isBackVisible ? 180 : 0
     }
-    @State var degrees: Double = 0
     var body: some View {
-        VStack {
             ZStack {
-                Text(isBackVisible ? card.back : card.front)
-                    .font(.system(size: 24))
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity,
-                           maxHeight: .infinity,
-                           alignment: .topLeading)
+                Group {
+                    Text(card.back)
+                        .scaleEffect(x: -1)
+                        .opacity(isBackVisible ? 1 : 0)
+                    
+                    Text(card.front)
+                        .opacity(isBackVisible ? 0 : 1)
+                }
+                .font(.system(size: 24))
+                .bold()
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity,
+                       maxHeight: .infinity,
+                       alignment: .topLeading)
+                
                 Button {
-                    isBackVisible.toggle()
+                    withAnimation{
+                        isBackVisible.toggle()
+                    }
+                    
                 } label: {
                     Image(systemName: "arrow.left.arrow.right.circle.fill")
                         .font(.system(size: 36))
@@ -34,12 +42,11 @@ struct CardView: View {
                 .frame(maxWidth: .infinity,
                        maxHeight: .infinity,
                        alignment: .bottomTrailing)
-                    
             }
             .padding()
             .aspectRatio(1.25, contentMode: .fit)
             .frame(width: 300, height: 225)
-            .background(Color.orange)
+            .background(isBackVisible ? .yellow : .pink)
             .cornerRadius(10)
             .shadow(radius: 8)
             .padding()
@@ -47,11 +54,7 @@ struct CardView: View {
                 .degrees(degrees),
                 axis: (x: 0.0, y: 1.0, z: 0.0)
             )
-        }
-        Text("Degrees: \(degrees)")
-        
-        Slider(value: $degrees, in: 0...180)
-            .padding(.horizontal)
+
     }
 }
 
